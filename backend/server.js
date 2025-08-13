e34152b82567ecae71fe28915e46d9375cd259c0
BlinkStar-Tech <blinkstardesigns@gmail.com>
2025-08-13 09:15:58 +0200

    adjusted my vercel.json

--- backend/server.js
+++ backend/server.js
@@ -57,4 +57,7 @@ app.use((err, req, res, next) => {
     });
 });
 
-module.exports = app;
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+}); 
